e4599de896c0e64c11b5597613a3c4f305b6f06e
LRAM878 <luisrarevalo2002@gmail.com>
2026-06-26 13:20:10 -0600

    Cambio a base de datos SQLite para Render

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'clave-super-secreta-heloim-2026'
     # Configuración para MySQL con PyMySQL
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/ferreteria_heloim'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///ferreteria.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
